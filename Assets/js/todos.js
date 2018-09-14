6a64dda253b6a31a4ad007b90eaf18fcdef1c719
JordanMichele <jordanmichele32@gmail.com>
2018-09-14 10:41:20 -0400

    finalized Goals for the day

--- Assets/js/todos.js
+++ Assets/js/todos.js
@@ -1,24 +1,30 @@
-// Check off Specific Todos by Clicking 
-$("ul").on("click", "li", function(){
-	$(this).toggleClass("completed");	
+// Check off Specific Todos by Clicking
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("completed");
 });
 
 //Click on X to delete Todo
-$("ul").on("click", "span", function(event){
-	$(this).parent().remove();
- 	event.stopPropagation();
+$("ul").on("click", "span", function(event) {
+  $(this)
+    .parent()
+    .remove();
+  event.stopPropagation();
 });
 
-$("input[type='text']").keypress(function(event){
-	if (event.which === 13) {
-		//grabbing new todo from input 
-		var todoText = $(this).val();
-		$(this).val("");
-		//create a new li and add to ul
-		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todoText + "</li>")
-	}
+//Select input text
+// Append text to UL when enterBtn(13) is pressed
+$("#inputBox").keypress(function(event) {
+  if (event.which === 13) {
+    //grabbing new todo from input
+    let todoText = $(this).val();
+    $(this).val("");
+    //create a new li and add to ul
+    $("ul").append(
+      "<li><span><i class='fa fa-trash'></i></span>" + todoText + "</li>"
+    );
+  }
 });
 
-$(".fa-bullseye").click(function(){
- $("input[type='text']").toggle("slow");
+$(".fa-bullseye").click(function() {
+  $("#inputBox").toggle("slow");
 });
